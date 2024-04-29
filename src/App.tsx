ab016abd11f563ff5b46ddd4721caba1fe507aa5
Ano Jumisa <anojumisa@Anos-MacBook-Pro.local>
2024-04-29 21:10:30 +0800

    Fetch API

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NewsList from './components/NewsList';
+
+function App() {
+  return (
+    <div className="App">
+      <NewsList />
+    </div>
+  );
+}
+
+export default App;
