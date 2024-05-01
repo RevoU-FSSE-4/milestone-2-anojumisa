12275dc2193c7fe81799b401d9ba9dafbff6c683
Ano Jumisa <anojumisa@gmx.de>
2024-05-01 11:27:20 +0800

    Direct news button to source link

--- src/Components/RightSection.tsx
+++ src/Components/RightSection.tsx
@@ -1,23 +1,39 @@
 import React from "react";
 
 type newsProp = {
-    news:any
-}
-const RightSection = (props:newsProp) => {
-    return (
-       <div className="w-56">
-            <h1>TOP NEWS</h1>
-            {props?.news?.map((data:any) => {
-                return <>
-                <ul className=" max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
-                    <li className="font-semibold text-gray-900 dark:text-black text-pretty">{data?.title}</li>
-                </ul>
-                <br className="mt-4"/>
-                </>
-            })}
-       </div>
-    )
-}
+	news: any;
+};
+const RightSection = (props: newsProp) => {
+	const topNews = props.news.slice(0, 10);
+	return (
+		<div className="w-56">
+			<h1>TOP NEWS</h1>
+			<ul className="max-w-md space-y-1 text-gray-500 dark:text-gray-400">
+				{topNews.map(
+					(data: { title: string }, index: React.Key | null | undefined) => (
+						<li
+							key={index}
+							className="font-semibold text-left text-gray-900 dark:text-black text-pretty list-outside"
+						>
+							{(index as number) + 1}. {data.title} {/* Type assertion */}
+						</li>
+					)
+				)}
+			</ul>
+		</div>
+		//    <div className="w-56">
+		//         <h1>TOP NEWS</h1>
+		//         {props?.news?.map((data:any) => {
+		//             return <>
+		//             <ul className=" max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
 
-export default RightSection
+		//                 <li className="font-semibold text-left text-gray-900 dark:text-black text-pretty">{data?.title}</li>
+		//             </ul>
+		//             <br className="mt-4"/>
+		//             </>
+		//         })}
+		//    </div>
+	);
+};
 
+export default RightSection;
